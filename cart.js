1219aa7eb047a7e6f372042b62a9de6ed77af840
lpattis1 <lpattis1@asu.edu>
2021-06-22 16:31:26 -0500

    Added remove buttons to order items, along with the function of totaling up all the items (and updating the total when items are removed) and implemented smooth scrolling.

--- cart.js
+++ cart.js
@@ -1,20 +1,27 @@
+// Cart arrays
 let cartItems = [];
 let cartPrices = [];
+let cartImages = [];
+// Cart state
 const userCart = {
   img: "/img/fishandchips.png",
   name: "",
   price: "",
 };
 
-function addToCart() {
+// Add and remove items from cart function
+function addRemoveCartItems() {
   // Variables:
 
   // Cart
   const cart = document.querySelector(".cart");
+  const cartClose = document.querySelector(".cart-modal-close");
   const count = document.querySelector(".count");
   let countNum = parseInt(count.textContent);
-
   const orders = document.querySelectorAll(".order-btn");
+  const ordersUl = document.querySelector(".orders");
+  const totalOrder = document.querySelector(".total-number");
+  let totalOrderPrice = parseFloat(totalOrder.textContent);
 
   // Item Categories
   const categories = document.querySelectorAll(".category");
@@ -33,7 +40,7 @@ function addToCart() {
 
   orderNowBtns.forEach((order) => {
     order.addEventListener("click", function (e) {
-      const ordersUl = document.querySelector(".orders");
+      // Added and manipulated cart variables
       const cartItem = order.parentElement;
       const cartItemName = cartItem.children[1].textContent;
       const cartItemPrice = cartItem.children[1].dataset.price;
@@ -41,23 +48,84 @@ function addToCart() {
       userCart.name = cartItemName;
       userCart.img = cartItemImg;
       userCart.price = parseInt(cartItemPrice);
+
       //   --------------------------
+
+      //   Adding cart properties to the cart array
       cartItems.push(userCart.name);
       cartPrices.push(userCart.price);
+      cartImages.push(userCart.img);
       countNum += cartItems.length;
 
+      //   Show how many items are in the cart currently
       if (countNum > 0) {
         count.classList.add("count-active");
         count.textContent = cartItems.length;
       } else {
         count.classList.remove("count-active");
       }
-      console.log(cartItems);
-      console.log(cartPrices);
 
-      //   <li><img src="/img/coleslaw.png" alt=""> gfgf</li>
+      //   Append cart items to the cart modal
+      let html = "";
+      html = `
+      <li class="order-item">
+      <img src="${userCart.img}" alt=""> ${userCart.name} 
+      <span class ="item-price">$<p>${userCart.price}</p></span>
+      <button class="remove-btn">Remove</button>
+      </li>
+      `;
+      ordersUl.innerHTML += html;
+
+      //   Add up total price in cart
+      totalOrderPrice = cartPrices.reduce((a, b) => {
+        return a + b;
+      });
+      totalOrder.textContent = totalOrderPrice;
+
+      //   Remove order items from cart and update price total
+      const orderItems = document.querySelectorAll(".order-item");
+      const removeOrderItem = document.querySelectorAll(".remove-btn");
+      removeOrderItem.forEach((remove) => {
+        remove.addEventListener("click", function (e) {
+          remove.parentElement.remove(remove.parentElement);
+          const removed =
+            remove.parentElement.children[1].children[0].textContent;
+          totalOrder.textContent = parseInt(totalOrder.textContent - removed);
+        });
+      });
     });
+  });
+
+  //   Open cart modal
+  cart.addEventListener("click", function (e) {
+    const modalBg = document.querySelector(".cart-modal-bg");
+    modalBg.classList.add("show-modal");
+  });
+
+  //   Close cart modal
+  cartClose.addEventListener("click", function (e) {
+    const modalBg = document.querySelector(".cart-modal-bg");
+    modalBg.classList.remove("show-modal");
   });
 }
 
-addToCart();
+addRemoveCartItems();
+
+// Checkout function - shows next two screens
+function checkout() {
+  const checkoutBtn = document.querySelector(".to-details-btn");
+
+  checkoutBtn.addEventListener("click", function (e) {
+    const totalOrder = document.querySelector(".total-number");
+    const amountAlert = document.querySelector(".amount-alert");
+    console.log(totalOrder);
+    let totalOrderPrice = parseFloat(totalOrder.textContent);
+    if (totalOrderPrice <= 0) {
+      amountAlert.classList.add("show-alert");
+    } else {
+      amountAlert.classList.remove("show-alert");
+    }
+  });
+}
+
+checkout();
